d3dc40b3a41730d38570236ec9d926128e2913fd
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-05-23 14:54:52 +1000

    Create Java project

---
+++ RecipeSaverApp/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jsoup:jsoup:1.17.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
